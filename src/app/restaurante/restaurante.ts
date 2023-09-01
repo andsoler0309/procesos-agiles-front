02ea1f7526da69b8d4f4ce34e0b399633d0338b9
Andres Soler <andres.soler@talent.com>
2023-09-01 10:15:52 -0500

    updating the restaurante class in the tests

--- src/app/restaurante/restaurante.ts
+++ src/app/restaurante/restaurante.ts
@@ -1,33 +1,50 @@
+import { Usuario } from "../usuario/usuario";
+
 export class Restaurante {
   id: number;
   nombre: string;
   direccion: string;
   telefono: string;
   facebook: string;
-  instagram: string;
   twitter: string;
+  instagram: string;
+  hora_atencion: string;
+  is_en_lugar: boolean;
+  is_domicilios: boolean;
   tipo_comida: string;
-  aplicaciones: string[];
+  is_rappi: boolean;
+  is_didi: boolean;
+  administrador: Usuario;
 
   public constructor(
-      id: number,
-      nombre: string,
-      direccion: string,
-      telefono: string,
-      facebook: string,
-      instagram: string,
-      twitter: string,
-      tipo_comida: string,
-      aplicaciones: string[]
+    id: number,
+    nombre: string,
+    direccion: string,
+    telefono: string,
+    facebook: string,
+    twitter: string,
+    instagram: string,
+    hora_atencion: string,
+    is_en_lugar: boolean,
+    is_domicilios: boolean,
+    tipo_comida: string,
+    is_rappi: boolean,
+    is_didi: boolean,
+    administrador: Usuario
   ) {
-      this.id = id;
-      this.nombre = nombre;
-      this.direccion = direccion;
-      this.telefono = telefono;
-      this.facebook = facebook;
-      this.instagram = instagram;
-      this.twitter = twitter;
-      this.tipo_comida = tipo_comida;
-      this.aplicaciones = aplicaciones;
+    this.id = id;
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.facebook = facebook;
+    this.twitter = twitter;
+    this.instagram = instagram;
+    this.hora_atencion = hora_atencion;
+    this.is_en_lugar = is_en_lugar;
+    this.is_domicilios = is_domicilios;
+    this.tipo_comida = tipo_comida;
+    this.is_rappi = is_rappi;
+    this.is_didi = is_didi;
+    this.administrador = administrador;
   }
 }
