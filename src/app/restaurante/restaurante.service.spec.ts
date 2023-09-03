7214b5a39f64f8e2fbc2947f1f9e777c24068ec4
Inés Rojas <ia.rojas2@uniandes.edu.co>
2023-09-02 23:42:39 -0500

    add new tests

--- src/app/restaurante/restaurante.service.spec.ts
+++ src/app/restaurante/restaurante.service.spec.ts
@@ -8,17 +8,33 @@ import { HttpClientModule } from '@angular/common/http';
 import { RestauranteListaComponent } from './restaurante-lista/restaurante-lista.component';
 import { ToastrModule } from 'ngx-toastr';
 import { Restaurante } from './restaurante';
+import { Usuario } from '../usuario/usuario';
 
 describe('Service: Restaurante', () => {
+  let service: RestauranteService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, HttpClientModule, ToastrModule.forRoot()],
       providers: [RestauranteService],
       declarations: [RestauranteListaComponent, AppComponent]
     });
+    service = TestBed.inject(RestauranteService);
   });
 
   it('should ...', inject([RestauranteService], (service: RestauranteService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('Should create restaurante from Http post call.',  inject([RestauranteService],(service: RestauranteService)  => {
+    service.crearRestaurante(
+      new Restaurante(0, "", "", "","","","","",true,true,"",true, true, new Usuario("",""))
+    )
+      .subscribe({
+        next: (response) => {
+          expect(response).toBeTruthy();
+        }
+      });
+  }));
+
 });
