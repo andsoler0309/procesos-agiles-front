61c529668dd11d6cb93866817714c3e884983683
Andres Soler <andres.soler@talent.com>
2023-09-12 11:48:20 -0500

    added code for edit and delete the restaurant

--- src/app/restaurante/restaurante-lista/restaurante-lista.component.ts
+++ src/app/restaurante/restaurante-lista/restaurante-lista.component.ts
@@ -14,6 +14,7 @@ export class RestauranteListaComponent implements OnInit {
   restaurantes:Array<Restaurante> = []
   error: string;
   restauranteElegido: Restaurante;
+  deleteButtonClicked: Boolean = false;
 
   constructor(
     private routerPath: Router,
@@ -54,7 +55,37 @@ export class RestauranteListaComponent implements OnInit {
   }
 
   verDetalle(restaurante: Restaurante): void {
+    if (this.restauranteElegido && this.restauranteElegido.id === restaurante.id) {
+      this.restauranteElegido = null;
+      return;
+    }
     this.restauranteElegido = restaurante
   }
 
+  editarRestaurante(restuanteId: Number): void {
+    this.routerPath.navigate(['/restaurantes/editar/' + restuanteId]);
+  }
+
+  borrarRestaurante(restuanteId: Number): void {
+    this.restauranteService.borrarRestaurante(restuanteId).subscribe((restaurante) => {
+    this.toastr.success("Éxito","Restaurante borrado correctamente")
+    this.ngOnInit();
+    },
+    error => {
+      if (error.statusText === "UNAUTHORIZED") {
+        if (error.error === "Solo los Administradores pueden borrar Restaurantes") {
+          this.toastr.error("Error","Solo los Administradores pueden borrar Restaurantes")
+          this.error = error.error;
+        } else {
+          this.toastr.error("Error","Su sesión ha caducado, por favor vuelva a iniciar sesión.")
+        }
+      }
+      else if (error.statusText === "UNPROCESSABLE ENTITY") {
+        this.toastr.error("Error","No hemos podido identificarlo, por favor vuelva a iniciar sesión.")
+      }
+      else {
+        this.toastr.error("Error","Ha ocurrido un error. " + error.message)
+      }
+    });
+  }
 }
